85943d5f1ef6f9a11d81beffcae878ac694ed35b
John Thompson <john@springframework.guru>
2022-06-09 13:35:49 -0400

    adding lazy init error example

--- src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
@@ -1,7 +1,11 @@
 package guru.springframework.orderservice.bootstrap;
 
+import guru.springframework.orderservice.domain.OrderHeader;
+import guru.springframework.orderservice.repositories.OrderHeaderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by jt on 6/8/22.
@@ -9,8 +13,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class Bootstrap implements CommandLineRunner {
 
+    @Autowired
+    OrderHeaderRepository orderHeaderRepository;
+
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("I was called!");
+        OrderHeader orderHeader = orderHeaderRepository.findById(1L).get();
+
+        orderHeader.getOrderLines().forEach(ol -> {
+            System.out.println(ol.getProduct().getDescription());
+
+            ol.getProduct().getCategories().forEach(cat -> {
+                System.out.println(cat.getDescription());
+            });
+        });
     }
 }
